ee3f77b0bb17dc9b419aca2e4d5c10f91044591d
trganda <jongsx1203@gmail.com>
2023-11-28 10:26:55 +0800

    fix DavLock error

--- buildenv/webdav/docker-entrypoint.sh
+++ buildenv/webdav/docker-entrypoint.sh
@@ -10,17 +10,16 @@ if [ ! -e "/usr/local/apache2/webdav.password" ]; then
     fi
 fi
 
-# Create directories for Dav data and lock database.
-[ ! -d "/var/lib/webdav/data" ] && mkdir -p "/var/lib/webdav/data"
-[ ! -e "/var/lib/webdav/DavLock.db" ] && touch "/var/lib/webdav/DavLock.db"
-chown -R www-data:www-data "/var/lib/webdav"
-
 # Generate self-signed SSL certificate.
 # If SERVER_NAMES is given, use the first domain as the Common Name.
 if [ ! -e /usr/local/apache2/conf/privkey.key ] || [ ! -e /usr/local/apache2/conf/cert.crt ]; then
     openssl req -x509 -newkey rsa:2048 -days 1000 -nodes \
         -keyout /usr/local/apache2/conf/privkey.key -out /usr/local/apache2/conf/cert.crt -subj "/CN=CN"
 fi
+
+# Create directories for Dav data and lock database.
+[ ! -d "/var/lib/webdav/data" ] && mkdir -p "/var/lib/webdav/data"
+chown www-data:www-data "/var/lib/webdav"
 
 # Run the command given in the Dockerfile at CMD
 exec "$@"
